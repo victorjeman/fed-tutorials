4dd30afaa2f53be7764d10a54e1f9e54978f3218
Victor JEMAN <victorjeman@gmail.com>
2017-02-08 10:27:34 +0200

    Added first demo

---
+++ demo/scrolltop/app.js
@@ -0,0 +1,71 @@
+/*jshint esversion: 6 */
+
+// ARTICLES:
+//
+// https://medium.freecodecamp.com/3-questions-to-watch-out-for-in-a-javascript-interview-725012834ccb#.76wyogqpe
+
+// TODO:
+// TODO 1: Make use of Debouncing for this here
+
+(($) => {
+
+  'use strict';
+
+  // ----------------------------------------------------------------
+  // CACHE THE DOM
+  // ----------------------------------------------------------------
+
+  let $scrollButton = $('.js-scroll-top-btn');
+  let $document = $(document);
+
+
+
+
+
+  // ----------------------------------------------------------------
+  // ADD LISTENERS
+  // ----------------------------------------------------------------
+
+  $scrollButton.on('click', scrollTop);
+
+  // [TODO 1]
+  $document.on('scroll', toggleScrollButton);
+
+
+
+
+
+  // ----------------------------------------------------------------
+  // METHODS
+  // ----------------------------------------------------------------
+
+  // Scroll the page
+  function scrollTop() {
+
+    let duration = 1000;
+    let scrollTop = 0;
+
+    $('html, body').animate({
+      scrollTop: scrollTop
+    }, duration);
+
+    console.log('LOG: SCROLLING TOP');
+
+  }
+
+
+  // Hide the scroll button when we are at the very top of the page
+  function toggleScrollButton() {
+
+    if ($document.scrollTop() > 80) {
+      $scrollButton.fadeIn("slow");
+    } else {
+      $scrollButton.fadeOut("slow");
+    }
+
+    console.log('LOG: WATCHING SCROLL EVENT');
+
+  }
+
+
+})(jQuery);
